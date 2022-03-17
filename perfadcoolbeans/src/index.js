9b54958b69b1fc7104b27e9d312f1d64128f6925
Neha Priyadarshini <npriyadarshini@twitter.com>
2022-03-17 14:54:39 -0700

    freha tag

--- perfadcoolbeans/src/index.js
+++ perfadcoolbeans/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Purchase from "./components/purchase";
-import Signup from "./components/signup";
 import Home from "./components/home";
 
 
@@ -13,7 +12,6 @@ ReactDOM.render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="purchase" element={<Purchase />} />
-        <Route path="signup" element={<Signup />} />
         <Route path="home" element={<Home />} />
       </Route>
     </Routes>
